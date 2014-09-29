c721c2793fa1141067bd0e80a8d17956b0e33e4c
Ethel Ng <ethel.ng@gmail.com>
2014-09-29 11:56:22 +0100

    Home page can now display the restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 describe 'restaurants' do
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    it 'should display restaurants' do
+      visit '/restaurants' 
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+
+  end
+
   context 'no restaurants added' do
     it 'should display a prompt to add a restaurant' do
       visit '/restaurants'
