24aee8f740f034432a40ec4134bec2bf20c5ec6a
Ethel Ng <ethel.ng@gmail.com>
2014-09-29 11:44:42 +0100

    Set up home page with a link to add new restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe 'restaurants' do
-  
+  context 'no restaurants added' do
+    it 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content "No restaurants"
+      expect(page).to have_link "Add a restaurant"
+    end
+  end
 end
