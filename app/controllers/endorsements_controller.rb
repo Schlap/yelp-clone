d3c58615037a1100f9dd9e0ce7b7ec3b06a69275
Ethel Ng <ethel.ng@gmail.com>
2014-10-01 18:07:39 +0100

    Included ActionView TextHelper in the endorsements controller so pluralizing now works nicely

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,9 +1,11 @@
 class EndorsementsController < ApplicationController
 
+  include ActionView::Helpers::TextHelper
+
   def create
     review = Review.find(params[:review_id])
     review.endorsements.create
-    render json: { new_endorsements_count: review.endorsements.count }
+    render json: { new_endorsements_count: pluralize(review.endorsements.count, 'endorsement') }
   end
 
 end
