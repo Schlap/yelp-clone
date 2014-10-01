70c790abde18c2a1c50df038dd02ab43f48e2083
Ethel Ng <ethel.ng@gmail.com>
2014-10-01 10:18:39 +0100

    Helper method for displaying the ratings as stars

---
+++ spec/helpers/reviews_helper_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe ReviewsHelper, type: :helper do
+  context 'star rating' do
+
+    it 'does nothing for an invalid number' do
+      expect(helper.star_rating('N/A')).to eq 'N/A'
+    end
+
+    it 'returns 5 black stars for 5' do
+      expect(helper.star_rating(5)).to eq '★★★★★'
+    end
+
+    it 'returns 3 black stars and 2 white stars for 3' do
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
+
+  end
+end
