e7a23ec9089af43cc673eb87a45fda3b89151e3d
Ethel Ng <ethel.ng@gmail.com>
2014-10-01 17:36:19 +0100

    Using AJAX post request when endorsing

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -2,11 +2,12 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-// $(document).ready(function(){
-//   $('.endorsements-link').on('click', function(event){
-//     event.preventDefault();
-//     $.post(this.href, function(response){
-//       $('.endorsements-count').text(response);
-//     });
-//   })
-// })
+$(document).ready(function(){
+  $('.endorsements-link').on('click', function(event){
+    var endorsementCount = $(this).siblings('.endorsements-count');
+    event.preventDefault();
+    $.post(this.href, function(response){
+      endorsementCount.text(response.new_endorsements_count);
+    });
+  })
+})
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
-  has_many :endorsements
+  has_many :endorsements, dependent: :destroy
   validates :rating, presence: true, inclusion: (1..5)
 end
