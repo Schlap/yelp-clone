090118a4e2274e275dc99f19a98aafc0754b7dac
Ethel Ng <ethel.ng@gmail.com>
2014-10-01 09:53:46 +0100

    Added validations for review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,1 +1,14 @@
-# don't allow anyone to enter a value that isn't 1 to 5
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  it 'is not valid without a rating' do
+    review = Review.new(comment: 'Average')
+    expect(review).to have(2).error_on(:rating)
+  end
+
+  it 'is not valid if the rating is not within 1 to 5' do
+    review = Review.new(comment: 'Average', rating: 6)
+    expect(review).to have(1).error_on(:rating)
+  end
+  
+end
