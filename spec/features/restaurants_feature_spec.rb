8b757d7074e88f4f83c29443f33841d9120d7bb3
Ethel Ng <ethel.ng@gmail.com>
2014-10-02 21:00:24 +0100

    Displaying restaurant cuisine and description on front page

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -2,22 +2,24 @@ require 'rails_helper'
 
 describe 'displaying restaurants' do
 
-  context 'no restaurants added' do
-    it 'should display a prompt to add a restaurant' do
+  context 'when no restaurants have been added' do
+    it 'displays a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content "No restaurants"
       expect(page).to have_link "Add a restaurant"
     end
   end
 
-  context 'restaurants have been added' do
+  context 'when restaurants have been added' do
     before do
       create :kfc
     end
 
-    it 'should display restaurants' do
+    it 'displays restaurants and their info' do
       visit '/restaurants' 
       expect(page).to have_content 'KFC'
+      expect(page).to have_content 'Fast food'
+      expect(page).to have_content 'Fried chicken'
       expect(page).not_to have_content 'No restaurants yet'
     end
   end
